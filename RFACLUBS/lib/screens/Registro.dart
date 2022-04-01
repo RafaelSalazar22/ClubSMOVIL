4d990f9ed2c5cfc8ab07f1b2895789d94f001e19
RafaelSalazar22 <91817560+RafaelSalazar22@users.noreply.github.com>
2022-03-31 23:43:57 -0600

    Add files via upload

---
+++ RFACLUBS/lib/screens/Registro.dart
@@ -0,0 +1,249 @@
+import 'package:club/Models/User.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
+
+import '../Models/ListResponse.dart';
+import '../Providers/UserProviders.dart';
+import 'login.dart';
+
+class Registro extends StatefulWidget {
+  const Registro({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<Registro> {
+  // our form key
+  final _formKey = GlobalKey<FormState>();
+  // editing Controller
+
+  @override
+  Widget build(BuildContext context) {
+     final Usuario usuario1 = Usuario(
+      idUser: 0,
+    );
+    final userProviders = Provider.of<UserProviders>(context);
+    final ListResponse usuario = userProviders.request;
+    //first name field
+    final firstNameField = TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.name,
+        initialValue: usuario1.nombre,
+        onChanged: (value) =>  usuario1.nombre = value,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          RegExp regex = RegExp(r'^.{3,}$');
+          if (value!.isEmpty) {
+            return ("El nombre no puede estar vacío");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Ingrese un nombre válido (mín. 3 caracteres)");
+          }
+          if (value.length > 20) {
+            return ("No  puede escribir mas de  20 caracteres.");
+          }
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.account_circle,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Nombre",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //second name field
+    final secondNameField = TextFormField(
+       initialValue: usuario1.apellidos,
+        onChanged: (value) =>  usuario1.apellidos = value,
+        autofocus: false,
+        keyboardType: TextInputType.name,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          // ignore: unused_local_variable
+          RegExp regex = RegExp(r'^.{3,}$');
+          if (value!.isEmpty) {
+            return ("El Apellido no puede estar vacío");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Ingrese un nombre válido (mín. 3 caracteres)");
+          }
+          if (value.length > 20) {
+            return ("No  puede escribir mas de  20 caracteres.");
+          }
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.account_circle,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Apellidos",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //email field
+    final emailField = TextFormField(
+       initialValue: usuario1.correo,
+        onChanged: (value) =>  usuario1.correo = value,
+        autofocus: false,
+        keyboardType: TextInputType.emailAddress,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Por favor introduzca su correo electrónico");
+          }
+          // reg expression for email validation
+          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+              .hasMatch(value)) {
+            return ("Por favor introduzca una dirección de correo electrónico válida");
+          }
+          if (value.length > 40) {
+            return ("No  puede escribir mas de  40 caracteres.");
+          }
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.mail,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Correo Electrónico ",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //password field
+    final passwordField = TextFormField(
+          initialValue: usuario1.password,
+        onChanged: (value) =>  usuario1.password = value,
+        autofocus: false,
+        obscureText: true,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          RegExp regex = RegExp(r'^.{6,}$');
+          if (value!.isEmpty) {
+            return ("Se requiere contraseña para iniciar sesión");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Introduzca una contraseña válida (mín. 6 caracteres)");
+          }
+          if (value.length > 12) {
+            return ("No  puede escribir mas de  12 caracteres.");
+          }
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.vpn_key,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Contraseña ",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //confirm password field
+    //signup button
+    final signUpButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(10),
+      color: Colors.redAccent,
+      child: MaterialButton(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              userProviders.post(usuario1);
+                 Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const loginScreen(),
+                    ));
+              
+              
+               
+              
+            }
+          },
+          child: const Text(
+            "Registrar",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.red),
+          onPressed: () {
+            // passing this to our root
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 180,
+                        child: Image.asset(
+                          "assets/Downloads/CLUB.png",
+                          fit: BoxFit.contain,
+                        )),
+                    const SizedBox(height: 45),
+                    firstNameField,
+                    const SizedBox(height: 20),
+                    secondNameField,
+                    const SizedBox(height: 20),
+                    emailField,
+                    const SizedBox(height: 20),
+                    passwordField,
+                    const SizedBox(height: 20),
+                    signUpButton,
+                    const SizedBox(height: 15),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ RFACLUBS/lib/screens/Torneo.dart
@@ -0,0 +1,330 @@
+import 'package:club/Models/Torneo.dart';
+import 'package:club/Providers/TorneoProviders.dart';
+import 'package:club/screens/blog.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Models/ListResponse.dart';
+
+// ignore: camel_case_types
+
+class torneo extends StatefulWidget {
+  const torneo({Key? key}) : super(key: key);
+
+  @override
+  _TorneosState createState() => _TorneosState();
+}
+
+class _TorneosState extends State<torneo> {
+  bool _switchHospitalizado = false;
+  final Torneo torneo1 =
+      Torneo(idTorneo: 0, cantidadEquipos: 0, cantidadRondas: 0);
+  final _formKey = GlobalKey<FormState>();
+  List<String> items = [
+    'Futbol',
+    'Beisbol',
+    'Basquetball',
+    'Natacion',
+    'Tenis',
+  ];
+  String? dropdownvalue = 'Futbol';
+  @override
+  Widget build(BuildContext context) {
+    final torneoProviders = Provider.of<TorneoProviders>(context);
+    final ListResponse torneo = torneoProviders.request;
+    torneo1.personasMovilidad = _switchHospitalizado;
+    torneo1.diciplina = dropdownvalue;
+    final enviar = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(10),
+      color: Colors.redAccent,
+      child: MaterialButton(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              torneoProviders.post(torneo1);
+              if (torneoProviders.request.exito == 1) {
+                Scaffold.of(context)
+                    // ignore: deprecated_member_use, unnecessary_const
+                    .showSnackBar(const SnackBar(
+                        content: Text('El torneo se  agrego exitosamente')));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => blog(),
+                    ));
+              } else {
+                Scaffold.of(context)
+                    // ignore: deprecated_member_use, unnecessary_const
+                    .showSnackBar(const SnackBar(
+                        content: Text('El torneo no se  agrego exitosamente')));
+              }
+            }
+          },
+          child: const Text(
+            "Registrar Torneo",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text(
+            "Registro de Torneo",
+          ),
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(
+            child: SingleChildScrollView(
+                child: Container(
+                    color: Colors.white,
+                    child: Padding(
+                        padding: const EdgeInsets.all(36.0),
+                        child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                SizedBox(
+                                    height: 180,
+                                    child: Image.asset(
+                                      "assets/Downloads/CLUB.png",
+                                      fit: BoxFit.contain,
+                                    )),
+                                _nombretorneo(), // TextField con múltiples opciones de decoración
+                                const SizedBox(height: 20),
+                                _cantidadequipo(),
+                                const SizedBox(height: 20),
+                                _numrondas(),
+                                const SizedBox(height: 20),
+                                _diciplina(),
+                                const SizedBox(height: 20),
+                                _direccionTorneo(),
+                                const SizedBox(height: 20),
+                                _personasdife(),
+                                const SizedBox(height: 20),
+                                enviar
+                              ],
+                            )))))));
+  }
+
+  Widget _personasdife() {
+    return SwitchListTile(
+        value: _switchHospitalizado,
+        activeColor: const Color.fromARGB(255, 218, 181, 15),
+        title: const Text(
+          '¿Aceptan personas con movilidad limitada?',
+          textAlign: TextAlign.left,
+          style: TextStyle(color: Colors.black, fontSize: 15),
+        ),
+        secondary: const Icon(Icons.accessibility),
+        onChanged: (valor) {
+          setState(() {
+            _switchHospitalizado = valor;
+          });
+        });
+  }
+
+  // Widget _horariotorneo() {
+  //   return TextFormField(
+  //       initialValue: torneo1.diciplina,
+  //       onChanged: (value) => torneo1.direccion = value,
+  //       autofocus: false,
+  //       keyboardType: TextInputType.datetime,
+  //       style: const TextStyle(color: Colors.black),
+  //       validator: (value) {
+  //         if (value!.isEmpty) {
+  //           return ("Este campo es obligatorio");
+  //         }
+  //         if (value.length > 6) {
+  //           return ("No  puede escribir mas de  6 caracteres.");
+  //         }
+  //         return null;
+  //       },
+  //       textInputAction: TextInputAction.next,
+  //       decoration: InputDecoration(
+  //         prefixIcon: const Icon(
+  //           Icons.date_range,
+  //           color: Colors.grey,
+  //         ),
+  //         contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+  //         hintText: "Horario Ejemplo: 12:00",
+  //         hintStyle: const TextStyle(color: Colors.grey),
+  //         border: OutlineInputBorder(
+  //           borderRadius: BorderRadius.circular(10),
+  //         ),
+  //       ));
+  // }
+
+  Widget _diciplina() {
+    return DropdownButtonFormField<String>(
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(width: 1, color: Colors.black))),
+      focusColor: Colors.white,
+      iconDisabledColor: Colors.black,
+      iconEnabledColor: Colors.black,
+      dropdownColor: Colors.white,
+      value: dropdownvalue,
+      // Down Arrow Icon
+      icon: const Icon(Icons.keyboard_arrow_down),
+      items: items
+          .map((items) => DropdownMenuItem<String>(
+                value: items,
+                child: Text(items,
+                    style: const TextStyle(fontSize: 15, color: Colors.black)),
+              ))
+          .toList(),
+      onChanged: (items) => setState(() => dropdownvalue = items!),
+    );
+  }
+
+  Widget _cantidadequipo() {
+    return TextFormField(
+        initialValue: torneo1.cantidadEquipos.toString(),
+        onChanged: (value) => torneo1.cantidadEquipos = int.parse(value),
+        autofocus: false,
+        keyboardType: TextInputType.number,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Este campo es obligatorio");
+          }
+          if (value.length > 3) {
+            return ("No  puede escribir mas de  3 caracteres.");
+          }
+          final n = num.tryParse(value);
+          if (n == 0 || n == null) {
+            return 'El numero de equipos debe de ser mayor a 0';
+          }
+          if (n <= 1) {
+            return 'El numero de equipos debe de ser mayor a 1 ';
+          }
+          // reg expression for email validation
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.people_alt,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Cantidad de equipos que pueden participar",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+  }
+
+  Widget _numrondas() {
+    return TextFormField(
+        autofocus: false,
+        keyboardType: TextInputType.number,
+        style: const TextStyle(color: Colors.black),
+        initialValue: torneo1.cantidadRondas.toString(),
+        onChanged: (value) => torneo1.cantidadRondas = int.parse(value),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Este campo es obligatorio");
+          }
+          if (value.length > 3) {
+            return ("No  puede escribir mas de  3 caracteres.");
+          }
+          final n = num.tryParse(value);
+          if (n == 0 || n == null || n <1) {
+            return 'El numero de rondas debe de ser mayor a 0';
+          }
+
+          // reg expression for email validation
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.sports_hockey_outlined,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Cantidad de rondas del torneo",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+  }
+
+  Widget _direccionTorneo() {
+    return TextFormField(
+        initialValue: torneo1.direccion,
+        onChanged: (value) => torneo1.direccion = value,
+        autofocus: false,
+        keyboardType: TextInputType.streetAddress,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Este campo es obligatorio");
+          }
+          if (value.length > 40) {
+            return ("No  puede escribir mas de  40 caracteres.");
+          }
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.add_business_rounded,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Dirección: Ejemplo: Calle 48 x 173 y 175",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+  }
+
+  Widget _nombretorneo() {
+    return TextFormField(
+        initialValue: torneo1.nombreTorneo,
+        onChanged: (value) => torneo1.nombreTorneo = value,
+        autofocus: true,
+        keyboardType: TextInputType.name,
+        style: const TextStyle(color: Colors.black),
+        validator: (value) {
+          RegExp regex = RegExp(r'^.{3,}$');
+          if (value!.isEmpty) {
+            return ("Este campo es obligatorio");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter Valid name(Min. 3 Character)");
+          }
+          if (value.length > 20) {
+            return ("No  puede escribir mas de  20 caracteres.");
+          }
+          return null;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.tour_sharp,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Nombrel del Torneo",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+  }
+}
